2799d4adf21e5403c40993eebade246d8009c47f
root <root@filterdecoder.2edv3e4gnoyu3chcfrztyzd00b.shax.internal.chinacloudapp.cn>
2020-09-15 09:17:06 +0000

    update code

--- frameSelector.py
+++ frameSelector.py
@@ -26,20 +26,22 @@ def set_up_argument(parser):
 def moveFile(fileDir,output_dir,sn):
     picknumber = 0
     pathDir = os.listdir(fileDir)  # 取图片的原始路径
+    #print(pathDir)
     os.makedirs(output_dir, exist_ok=True)
-    if type(sn)==int:
-        picknumber = sn # sn为整数，则抽取相应数量的图图片
-    if type(sn)==float:
+    #print(output_dir)
+    if sn[0]!='0':
+        picknumber = int(sn) # sn为整数，则抽取相应数量的图图片
+    if sn[0]=='0':
         filenumber = len(pathDir)
-        rate = sn  # 自定义抽取图片的比例，sn为浮点型，则抽取相应比例的图片。
+        rate = float(sn)  # 自定义抽取图片的比例，sn为浮点型，则抽取相应比例的图片。
         picknumber = int(filenumber * rate)  # 按照rate比例从文件夹中取一定数量图片
-
+    print("从{}张图中共抽取{}张图片".format(len(pathDir),picknumber))
     sample = random.sample(pathDir, picknumber)  # 随机选取picknumber数量的样本图片
-
+    #print(sample)
     for name in tqdm(sample):
         move_Dir, tar_Dir = Path(fileDir, name), Path(tarDir, name)
-        print(move_Dir,tar_Dir)
         shutil.move(move_Dir, tar_Dir)
+    print('Done!')
     return
 
 
@@ -49,7 +51,8 @@ if __name__ == '__main__':
     fileDir = select_args.sourceFileDir  # 源图片文件夹路径
     tarDir = select_args.saveResultDir  # 移动到新的文件夹路径
     sn = select_args.select_number
-    print(fileDir,tarDir,sn)
+    #print(fileDir,tarDir,sn)
+    #print(type(sn))
     moveFile(fileDir, tarDir, sn)
 
 
