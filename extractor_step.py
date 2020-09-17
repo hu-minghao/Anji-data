9189cefac679e89cb11e6f723ab7c99ccf3c2580
hu-minghao <530019431@qq.com>
2020-09-17 15:35:18 +0800

    add select camera

--- extractor_step.py
+++ extractor_step.py
@@ -12,7 +12,7 @@ def main():
 
     # The location of the root directory to extract videos from
     root_dir = r"C:\video_clips"
-    select_camera = ['camera30','camera31','camera29','camera32']
+    select_camera = {'camera30','camera31','camera29','camera32'}
     camera_date_select = Dater(root_dir).date
     for camera in camera_date_select:
         print('get clip from {} in {}'.format(camera, camera_date_select[camera]))
@@ -23,49 +23,53 @@ def main():
     # start_time, end_time = 202009150300, 202009150305
     init_num = -1
     for camera_dir in Path(root_dir).iterdir():
-        for datetime_dir in camera_dir.iterdir():
-            if Path(datetime_dir).is_dir():
-                dir_time = int(datetime_dir.name[-4:])
-            else:
-                continue
-            if (dir_time >= 2300) or (dir_time <= 1100):
-                for video_file_path in datetime_dir.iterdir():
-                    # If the file is a hidden file
-                    if video_file_path.stem[0] == ".":
-                        continue
-                    else:
-                        init_num +=1
-                    # cv2.VideoCapture requires a string argument
-                    video_file = str(video_file_path)
+        print('camera name',camera_dir.name)
+        if camera_dir.name in select_camera:
+            for datetime_dir in camera_dir.iterdir():
+                if Path(datetime_dir).is_dir():
+                    dir_time = int(datetime_dir.name[-4:])
+                else:
+                    continue
+                if (dir_time >= 2300) or (dir_time <= 1100):
+                    for video_file_path in datetime_dir.iterdir():
+                        # If the file is a hidden file
+                        if video_file_path.stem[0] == ".":
+                            continue
+                        else:
+                            init_num +=1
+                        # cv2.VideoCapture requires a string argument
+                        video_file = str(video_file_path)
 
-                    if init_num%INTERVAL_CLIP == 0:
-                        print('this is number {} clips, the path is {}'.format(init_num,video_file))
-                        captured_video = cv2.VideoCapture(video_file)
+                        if init_num%INTERVAL_CLIP == 0:
+                            print('this is number {} clips, the path is {}'.format(init_num,video_file))
+                            captured_video = cv2.VideoCapture(video_file)
 
-                        # Gets the frames per second of this file
-                        fps = captured_video.get(cv2.CAP_PROP_FPS)
+                            # Gets the frames per second of this file
+                            fps = captured_video.get(cv2.CAP_PROP_FPS)
 
-                        interval_frames = INTERVAL_FRAME * fps
+                            interval_frames = INTERVAL_FRAME * fps
 
-                        frame_no = -1
-                        while captured_video.isOpened():
-                            read_successful, frame = captured_video.read()
+                            frame_no = -1
+                            while captured_video.isOpened():
+                                read_successful, frame = captured_video.read()
 
-                            # Stop reading if failed this frame
-                            if not read_successful:
-                                break
+                                # Stop reading if failed this frame
+                                if not read_successful:
+                                    break
 
-                            frame_no += 1
-                            if frame_no % interval_frames == 0:
+                                frame_no += 1
+                                if frame_no % interval_frames == 0:
 
-                                # Output
-                                img_file = str(os.path.join(
-                                    output_dir,
-                                    camera_dir.stem + "_" + datetime_dir.stem + "_" \
-                                        + Path(video_file).stem + "_" + str(frame_no) \
-                                        + ".jpg"
-                                ))
-                                cv2.imwrite(img_file, frame)
+                                    # Output
+                                    img_file = str(os.path.join(
+                                        output_dir,
+                                        camera_dir.stem + "_" + datetime_dir.stem + "_" \
+                                            + Path(video_file).stem + "_" + str(frame_no) \
+                                            + ".jpg"
+                                    ))
+                                    cv2.imwrite(img_file, frame)
+        else:
+            continue
 
 
 if __name__ == "__main__":
